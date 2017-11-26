33f3ef871d8f1ebb055b46ee097cafe09fb1d0ef
ly <ly@daier>
2017-11-26 21:46:36 +0800

    1.1

--- aps/src/main/java/com/jtd/config/MainConfig.java
+++ aps/src/main/java/com/jtd/config/MainConfig.java
@@ -22,7 +22,7 @@ public class MainConfig extends JFinalConfig {
 
 	/**
 	 * 运行此 main 方法可以启动项目，此main方法可以放置在任意的Class类定义中，不一定要放于此
-	 * 
+	 * ?????????????????????????
 	 * 使用本方法启动过第一次以后，会在开发工具的 debug、run config 中自动生成
 	 * 一条启动配置，可对该自动生成的配置再添加额外的配置项，例如 VM argument 可配置为：
 	 * -XX:PermSize=64M -XX:MaxPermSize=256M
